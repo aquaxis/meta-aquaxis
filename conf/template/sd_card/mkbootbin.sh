bc7e7ff5257b717cf0ad94b9d0d697d12da66b91
Hidemi Ishihara <hidemi@aquaxis.com>
2020-06-10 14:14:54 +0900

    Update mkbootbin.sh

--- conf/template/sd_card/mkbootbin.sh
+++ conf/template/sd_card/mkbootbin.sh
@@ -6,14 +6,15 @@ fi
 
 #cp <VIVADO_DIR>/fsbl.elf .
 
-cp ../pmutmp/deploy/images/zynqmp-pmu/pmu-firmware-zynqmp-pmu.elf ./pmufw.elf
+cp ../pmutmp/deploy/images/microblaze-pmu/pmu-firmware-microblaze-pmu.elf ./pmufw.elf
 
 cp ../tmp/deploy/images/ultra96v2/arm-trusted-firmware.elf ./bl31.elf
 cp ../tmp/deploy/images/ultra96v2/u-boot.elf ./
 
-bootgen -arch zynqmp -image build_boot.bif -o i BOOT.BIN -w on
+bootgen -arch zynqmp -image build_boot.bif -o i boot.bin -w on
 
 cp ../tmp/deploy/images/ultra96v2/Image ./
-cp ../tmp/deploy/images/ultra96v2/zynqmp-ultra96-v2.dtb ./
+#cp ../tmp/deploy/images/ultra96v2/zynqmp-ultra96-v2.dtb ./
 
 cp ../tmp/deploy/images/ultra96v2/core-image-minimal-ultra96v2.tar.gz ./
+cp ../tmp/deploy/images/ultra96v2/core-image-minimal-ultra96v2.ext4 ./
